48c45ca109dd85b8dbd7a0081c8ffb04a7cd0d4c
Vinit Gupta <vinitman35@gmail.com>
2021-09-09 14:32:42 +0530

    add error message

--- login/login.js
+++ login/login.js
@@ -1,20 +1,27 @@
+import { validateEmail } from "../validator/email";
+
 const email = document.querySelector(".login-email");
 const password = document.querySelector(".login-password");
 const loginBtn = document.querySelector("#login-btn");
 
-loginBtn.addEventListener("click",handleLogin)
+loginBtn.addEventListener("click",handleLogin);
+
+email.addEventListener("change",);
+
+function checkEmail(){
+    
+}
 
 
 async function handleLogin(e){
     e.preventDefault();
-    console.log(email.value);
-    // await fetch(`http://localhost:3000/user/login/`,{
-    //     method : 'POST',
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     },
-    //     body : {
-    //         email : email.target.value
-    //     }
-    // })
+    await fetch(`http://localhost:3000/user/login/`,{
+        method : 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body : {
+            email : email.target.value
+        }
+    })
 }
