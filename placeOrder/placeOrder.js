438ff77ea973ee39beb0092e06d6bd4239df279d
Vinit Gupta <vinitman35@gmail.com>
2021-09-10 19:30:24 +0530

    increase and decrease count button

--- placeOrder/placeOrder.js
+++ placeOrder/placeOrder.js
@@ -1,4 +1,6 @@
 const cartIds = JSON.parse(window.localStorage.getItem("cart"));
+const container = document.querySelector(".container");
+const loader = document.querySelector(".loader");
 let ids = [];
 (()=>{
     
@@ -27,7 +29,7 @@ async function getCartItems(){
     })
     .then((cartItems)=>{
         console.log(cartItems);
-
+        displayCartItems(cartItems.products);
     })
     .catch((err)=>{
         console.error(err);
@@ -35,6 +37,33 @@ async function getCartItems(){
 }
 
 
-function displayCart(){
-    
+function displayCartItems(cartItems){
+    let cartDisplay = document.createElement("div");
+    cartDisplay.className = "cart-display";
+    console.log(cartItems)
+    cartItems.forEach((item,index)=>{
+        console.log(item)
+        let cartItem = document.createElement("div");
+        cartItem.classList = "cart-item";
+        cartItem.innerHTML = `
+        <div class="cart-top">
+            <div class="cart-item-name">${item.name}</div>
+            <div class="cart-item-counter">
+                <div class="count-btn decrease-count">-</div>
+                <div class="cart-item-count">1</div>
+                <div class="count-btn increase-count">+</div>
+            </div>
+        </div>
+        <div class="cart-bottom">
+            <div class="cost-per-item">
+                &#8377;${item.cost}
+            </div>
+        </div>
+        `
+        cartDisplay.appendChild(cartItem);
+    })
+    container.appendChild(cartDisplay);
+    console.log(container)
+    loader.style.display = "none";
+    container.style.display = "inherit";
 }
